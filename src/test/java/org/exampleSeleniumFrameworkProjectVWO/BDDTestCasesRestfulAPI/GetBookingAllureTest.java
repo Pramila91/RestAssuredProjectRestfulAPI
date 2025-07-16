22b120d49c3d9a578e78a22a3ec7f95bc5343ba8
Pramila91 <pramila.lamba5@gmail.com>
2025-07-16 18:41:53 +0530

    fixes to GetBookingDataDrivenPositiveTests class

---
+++ src/test/java/org/exampleSeleniumFrameworkProjectVWO/BDDTestCasesRestfulAPI/GetBookingAllureTest.java
@@ -0,0 +1,27 @@
+package org.exampleSeleniumFrameworkProjectVWO.BDDTestCasesRestfulAPI;
+
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+@Epic("Booking API Tests")
+@Feature("GET Booking by ID")
+public class GetBookingAllureTest extends BaseTest {
+
+    @Test(description = "Verify valid booking ID returns booking details")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("This test validates that a valid booking ID returns 200 OK and expected fields.")
+    public void validBookingId_shouldReturnBookingDetails() {
+        given()
+                .when()
+                .get("/booking/25")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("firstname", notNullValue())
+                .body("lastname", notNullValue())
+                .body("bookingdates.checkin", notNullValue());
+    }
+}
---
+++ src/test/java/org/exampleSeleniumFrameworkProjectVWO/BDDTestCasesRestfulAPI/GetBookingDataDrivenPositiveTests.java
@@ -0,0 +1,50 @@
+package org.exampleSeleniumFrameworkProjectVWO.BDDTestCasesRestfulAPI;
+
+import io.qameta.allure.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetBookingDataDrivenPositiveTests extends BaseTest {
+
+    @DataProvider(name = "validBookingIds")
+    public Object[][] provideValidBookingIds() {
+        // Extract booking IDs as a List
+        List<Integer> ids = given()
+                .when()
+                .get("/booking")
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("bookingid"); // get all booking ids
+
+        // Get first 5 or fewer if not available
+        int size = Math.min(ids.size(), 5);
+        Object[][] data = new Object[size][1];
+        for (int i = 0; i < size; i++) {
+            data[i][0] = ids.get(i);
+        }
+        return data;
+    }
+
+    @Test(dataProvider = "validBookingIds")
+    @Epic("Booking API Tests")
+    @Feature("GET Booking by ID")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Test that valid booking IDs return HTTP 200 and booking details")
+    public void validBookingId_shouldReturn200AndDetails(int bookingId) {
+        given()
+                .when()
+                .get("/booking/" + bookingId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("firstname", notNullValue())
+                .body("bookingdates.checkin", notNullValue());
+    }
+}
+
